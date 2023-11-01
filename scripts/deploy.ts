e8045dea0a244adf26e8a745283355f863a58e3b
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2023-11-01 17:55:59 -0400

    init

---
+++ scripts/deploy.ts
@@ -0,0 +1,30 @@
+import { ethers } from "hardhat";
+import { build } from "./deploy-cannon";
+import synthetixDeployment from "../deployments/synthetix/CoreProxy.json";
+
+// To run:
+// cannon build synthetix-sandbox.toml --write-script ./cannon-deploy.ts --wipe
+// cannon inspect synthetix-sandbox --write-deployments ./deployments
+// npx hardhat node
+// npx hardhat run --network localhost scripts/deploy.ts
+
+async function main() {
+  const [signer] = await ethers.getSigners();
+
+  build(signer);
+
+  const { address, abi } = synthetixDeployment;
+
+  const coreProxy = new ethers.Contract(address, abi, signer);
+
+  await coreProxy.createAccount();
+
+  console.log("Successfully create account!");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
